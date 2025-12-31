7fd531588186b03504d24dd3f13a716ff2d137e5
Jude Boachie <jude.boachie@amalitech.com>
2025-12-31 10:43:42 +0000

    implement echo

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        let command = command.trim();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input: Vec<&str> = input.trim().split(" ").collect();
+        let command = input[0];
 
         if command.is_empty() {
             continue;
@@ -16,7 +17,16 @@ fn main() {
 
         match command {
             "exit" => break,
-            _ => eprintln!("{}: command not found", command.trim())
+            "echo" => {
+                for (idx, arg) in input[1..].iter().enumerate() {
+                    if idx > 0 {
+                        print!(" ")
+                    }
+                    print!("{}", arg);
+                }
+                println!();
+            }
+            _ => eprintln!("{}: command not found", command.trim()),
         }
     }
 }
