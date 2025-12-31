e777f1069b47da27b202d364c9fc8599d73993d6
Jude Boachie <jude.boachie@amalitech.com>
2025-12-31 15:34:43 +0000

    run a program

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ use std::env;
 use std::io::{self, Write};
 
 #[cfg(unix)]
-use std::os::unix::ffi::{OsStrExt};
+use std::os::unix::ffi::OsStrExt;
 #[cfg(unix)]
-use std::path::{Path};
+use std::path::Path;
+use std::process::Command;
 
 const BUILTINS: [&str; 3] = ["echo", "exit", "type"];
 
-enum Command {
+enum InputCommand {
     Exit,
     Type { input: String },
     Echo { input: String },
+    Executable { program: String, args: String },
     Unknown,
 }
 
-impl From<&str> for Command {
+impl From<&str> for InputCommand {
     fn from(value: &str) -> Self {
         match value {
             "exit" => Self::Exit,
@@ -25,13 +27,24 @@ impl From<&str> for Command {
             val if val.starts_with("type") => Self::Type {
                 input: val[4..].trim().to_string(),
             },
+            val if is_executable(val.split(" ").next().unwrap(), false) => {
+                let mut input = val.split(" ");
+                let program = input.next().unwrap();
+                let args = &val[program.len()..].trim();
+                println!("{}", args);
+
+                Self::Executable {
+                    program: String::from(program),
+                    args: args.to_string(),
+                }
+            }
             _ => Self::Unknown,
         }
     }
 }
 
 #[cfg(unix)]
-fn is_executable(path: &Path) -> anyhow::Result<bool> {
+fn is_unix_executable(path: &Path) -> anyhow::Result<bool> {
     use libc::{X_OK, access};
 
     let cstr = std::ffi::CString::new(path.as_os_str().as_bytes().to_vec())?;
@@ -39,12 +52,7 @@ fn is_executable(path: &Path) -> anyhow::Result<bool> {
     Ok(res == 0)
 }
 
-fn get_type(input: String) {
-    if BUILTINS.contains(&&*input) {
-        println!("{} is a shell builtin", input);
-        return;
-    }
-
+fn is_executable(input: &str, should_print: bool) -> bool {
     let path = std::env::var("PATH").unwrap();
     let path_entries = env::split_paths(&path);
 
@@ -66,9 +74,11 @@ fn get_type(input: String) {
         {
             let candidate = dir.join(&input);
             if candidate.is_file() {
-                match is_executable(&candidate) {
+                match is_unix_executable(&candidate) {
                     Ok(true) => {
-                        println!("{} is {}", input, candidate.display());
+                        if should_print {
+                            println!("{} is {}", input, candidate.display());
+                        }
                         found = true;
                         break;
                     }
@@ -82,7 +92,9 @@ fn get_type(input: String) {
             for ext in &path_exts {
                 let candidate = dir.join(format!("{}{}", input, ext));
                 if candidate.is_file() {
-                    println!("{} is {}", input, candidate.display());
+                    if should_print {
+                        println!("{} is {}", input, candidate.display());
+                    }
                     found = true;
                     break;
                 }
@@ -90,7 +102,16 @@ fn get_type(input: String) {
         }
     }
 
-    if !found {
+    found
+}
+
+fn get_type(input: String) {
+    if BUILTINS.contains(&&*input) {
+        println!("{} is a shell builtin", input);
+        return;
+    }
+
+    if !is_executable(&input, true) {
         println!("{}: not found", input)
     }
 }
@@ -109,13 +130,25 @@ fn main() {
             continue;
         }
 
-        let command = Command::from(input);
+        let command = InputCommand::from(input);
 
         match command {
-            Command::Exit => break,
-            Command::Echo { input } => println!("{}", input),
-            Command::Type { input } => get_type(input),
-            Command::Unknown => eprintln!("{}: command not found", command_str.trim()),
+            InputCommand::Exit => break,
+            InputCommand::Echo { input } => println!("{}", input),
+            InputCommand::Type { input } => get_type(input),
+            InputCommand::Executable { program, args } => {
+                let output = Command::new(program)
+                    .args(args.split(" "))
+                    .output()
+                    .expect("Failed to execute process");
+                if !output.stdout.is_empty() {
+                    println!("{}", String::from_utf8_lossy(&output.stdout));
+                }
+                if !output.stderr.is_empty() {
+                    eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+                }
+            }
+            InputCommand::Unknown => eprintln!("{}: command not found", command_str.trim()),
         }
     }
 }
